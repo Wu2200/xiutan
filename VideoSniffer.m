12f2c8ff1e9e1b6de72bb579f91e03a0f1c5dde5
无 <qw1917431753@gmail.com>
2026-09-05 19:03:21 +0800

    Update VideoSniffer.m

--- VideoSniffer.m
+++ VideoSniffer.m
@@ -11,11 +11,9 @@
 @end
 
 @interface SnifferOverlayWindow : UIWindow
-@property (nonatomic, strong) UIButton *roundIdleButton;
 @property (nonatomic, strong) UIView *buttonsContainer;
+@property (nonatomic, strong) UIButton *refreshButton;
 @property (nonatomic, assign) BOOL isSuspendedHidden;
-- (void)syncContainerPositionToIdleButton;
-- (void)syncIdleButtonPositionToContainer;
 @end
 
 @interface SnifferScriptBridge : NSObject <WKScriptMessageHandler>
@@ -23,6 +21,8 @@
 
 @interface SnifferManager : NSObject <UIGestureRecognizerDelegate>
 @property (nonatomic, copy) NSString *latestMediaUrl;
+@property (nonatomic, assign) NSInteger latestMediaScore;
+@property (nonatomic, assign) BOOL acceptsNextMediaUrl;
 @property (nonatomic, strong) SnifferOverlayWindow *overlayWindow;
 @property (nonatomic, strong) SnifferScriptBridge *scriptBridge;
 @property (nonatomic, weak) UIViewController *lastActiveVC;
@@ -53,15 +53,6 @@
             CGFloat ratio = [[SnifferManager sharedManager] loadPositionRatio];
             CGFloat targetCenterY = ratio * size.height;
 
-            UIButton *idleBtn = win.roundIdleButton;
-            if (idleBtn) {
-                CGFloat btnW = idleBtn.frame.size.width;
-                CGFloat btnH = idleBtn.frame.size.height;
-                CGFloat safeX = size.width - btnW - 10;
-                CGFloat safeY = MIN(MAX(targetCenterY - btnH / 2.0, 40), size.height - btnH - 40);
-                idleBtn.frame = CGRectMake(safeX, safeY, btnW, btnH);
-            }
-
             UIView *container = win.buttonsContainer;
             if (container) {
                 CGFloat w = container.frame.size.width;
@@ -76,55 +67,14 @@
 @end
 
 @implementation SnifferOverlayWindow
-
-- (void)syncContainerPositionToIdleButton {
-    if (!self.roundIdleButton || !self.buttonsContainer) {
-        return;
-    }
-    CGFloat centerY = self.roundIdleButton.center.y;
-    CGFloat screenH = self.bounds.size.height;
-    CGFloat h = self.buttonsContainer.frame.size.height;
-    CGFloat safeY = MIN(MAX(centerY - h / 2.0, 40), screenH - h - 40);
-    CGRect f = self.buttonsContainer.frame;
-    f.origin.y = safeY;
-    f.origin.x = self.bounds.size.width - f.size.width - 10;
-    self.buttonsContainer.frame = f;
-}
-
-- (void)syncIdleButtonPositionToContainer {
-    if (!self.roundIdleButton || !self.buttonsContainer) {
-        return;
-    }
-    CGFloat centerY = self.buttonsContainer.center.y;
-    CGFloat screenH = self.bounds.size.height;
-    CGFloat h = self.roundIdleButton.frame.size.height;
-    CGFloat safeY = MIN(MAX(centerY - h / 2.0, 40), screenH - h - 40);
-    CGRect f = self.roundIdleButton.frame;
-    f.origin.y = safeY;
-    f.origin.x = self.bounds.size.width - f.size.width - 10;
-    self.roundIdleButton.frame = f;
-}
-
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
-    if (self.isSuspendedHidden) {
+    if (self.isSuspendedHidden || !self.buttonsContainer || self.buttonsContainer.hidden || self.buttonsContainer.alpha < 0.05) {
         return nil;
     }
-
-    if (self.buttonsContainer && !self.buttonsContainer.hidden && self.buttonsContainer.alpha > 0.05) {
-        CGPoint containerPoint = [self convertPoint:point toView:self.buttonsContainer];
-        if ([self.buttonsContainer pointInside:containerPoint withEvent:event]) {
-            return [super hitTest:point withEvent:event];
-        }
-        return nil;
+    CGPoint containerPoint = [self convertPoint:point toView:self.buttonsContainer];
+    if ([self.buttonsContainer pointInside:containerPoint withEvent:event]) {
+        return [super hitTest:point withEvent:event];
     }
-
-    if (self.roundIdleButton && !self.roundIdleButton.hidden && self.roundIdleButton.alpha > 0.05) {
-        CGPoint btnPoint = [self convertPoint:point toView:self.roundIdleButton];
-        if ([self.roundIdleButton pointInside:btnPoint withEvent:event]) {
-            return [super hitTest:point withEvent:event];
-        }
-    }
-
     return nil;
 }
 @end
@@ -149,18 +99,20 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         inst = [[SnifferManager alloc] init];
+        inst.latestMediaScore = NSIntegerMinValue;
+        inst.acceptsNextMediaUrl = NO;
         inst.scriptBridge = [[SnifferScriptBridge alloc] init];
     });
     return inst;
 }
 
 - (void)savePositionRatio:(CGFloat)ratio {
-    [[NSUserDefaults standardUserDefaults] setDouble:ratio forKey:@"Sniffer_PositionRatio_Y"];
+    [[NSUserDefaults standardUserDefaults] setDouble:ratio forKey:@"Sniffer_Buttons_PositionRatio_Y"];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (CGFloat)loadPositionRatio {
-    double ratio = [[NSUserDefaults standardUserDefaults] doubleForKey:@"Sniffer_PositionRatio_Y"];
+    double ratio = [[NSUserDefaults standardUserDefaults] doubleForKey:@"Sniffer_Buttons_PositionRatio_Y"];
     if (ratio <= 0.05 || ratio >= 0.95) {
         return 0.5;
     }
@@ -232,12 +184,10 @@
             self.overlayWindow.isSuspendedHidden = NO;
             if (self.latestMediaUrl && self.latestMediaUrl.length > 0) {
                 [self showButtonsWithAnimation];
-            } else {
-                [self showIdleButtonWithAnimation];
             }
         } else {
             self.overlayWindow.isSuspendedHidden = YES;
-            [self hideAllFloatingUI];
+            [self hideButtonsWithAnimation];
         }
     }
 }
@@ -261,6 +211,61 @@
     [self attachGlobalToggleGesture];
 }
 
+- (BOOL)isMediaSegmentUrl:(NSString *)urlStr {
+    NSString *lower = [urlStr lowercaseString];
+    NSArray<NSString *> *segmentKeys = @[
+        @".ts",
+        @".m4s",
+        @".cmfv",
+        @".cmfa",
+        @".aac",
+        @".mp3",
+        @".vtt",
+        @".key",
+        @"segment",
+        @"chunk"
+    ];
+    for (NSString *key in segmentKeys) {
+        if ([lower containsString:key]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (NSInteger)mediaScoreForUrl:(NSString *)urlStr {
+    NSString *lower = [urlStr lowercaseString];
+    NSInteger score = 0;
+
+    if ([lower containsString:@".m3u8"] || [lower containsString:@"m3u8?"]) {
+        score += 1000;
+    } else if ([lower containsString:@".mpd"] || [lower containsString:@"manifest"]) {
+        score += 900;
+    } else if ([lower containsString:@".mp4"] || [lower containsString:@"videoplayback"]) {
+        score += 800;
+    } else {
+        score += 500;
+    }
+
+    if ([lower containsString:@"2160"] || [lower containsString:@"4k"] || [lower containsString:@"uhd"]) {
+        score += 500;
+    } else if ([lower containsString:@"1440"] || [lower containsString:@"2k"]) {
+        score += 400;
+    } else if ([lower containsString:@"1080"]) {
+        score += 300;
+    } else if ([lower containsString:@"720"]) {
+        score += 200;
+    } else if ([lower containsString:@"540"]) {
+        score += 150;
+    } else if ([lower containsString:@"480"]) {
+        score += 100;
+    } else if ([lower containsString:@"360"]) {
+        score += 50;
+    }
+
+    return score;
+}
+
 - (BOOL)isMediaUrl:(NSString *)urlStr {
     NSString *lower = [urlStr lowercaseString];
     NSArray *blackList = @[@".png", @".jpg", @".jpeg", @".gif", @".webp", @".css", @".js", @".svg", @".ico", @".woff", @".ttf"];
@@ -269,13 +274,33 @@
             return NO;
         }
     }
-    NSArray *keys = @[@"m3u8", @"mp4", @"flv", @"mov", @"mkv", @"webm", @"mpd", @"f4v", @"avi", @"ts", @"playlist", @"manifest", @"videoplayback", @"stream", @"video", @"live", @"vod", @"media", @"play"];
+
+    if ([self isMediaSegmentUrl:urlStr]) {
+        return NO;
+    }
+
+    NSArray *keys = @[@"m3u8", @"mp4", @"flv", @"mov", @"mkv", @"webm", @"mpd", @"f4v", @"avi", @"playlist", @"manifest", @"videoplayback", @"stream", @"video", @"live", @"vod", @"media", @"playurl", @"play_url", @"video_url"];
     for (NSString *key in keys) {
         if ([lower containsString:key]) {
             return YES;
         }
     }
     return NO;
+}
+
+- (void)rotateRefreshIcon {
+    UIButton *btn = self.overlayWindow.refreshButton;
+    if (!btn) {
+        return;
+    }
+
+    [btn.layer removeAnimationForKey:@"sniffer.refresh.rotation"];
+    CABasicAnimation *rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    rotation.fromValue = @(0.0);
+    rotation.toValue = @(M_PI * 6.0);
+    rotation.duration = 1.5;
+    rotation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    [btn.layer addAnimation:rotation forKey:@"sniffer.refresh.rotation"];
 }
 
 - (void)captureUrl:(NSString *)urlStr {
@@ -290,7 +315,27 @@
     }
 
     dispatch_async(dispatch_get_main_queue(), ^{
+        NSInteger incomingScore = [self mediaScoreForUrl:urlStr];
+        BOOL urlChanged = ![self.latestMediaUrl isEqualToString:urlStr];
+        BOOL shouldReplace = NO;
+
+        if (!self.latestMediaUrl || self.latestMediaUrl.length == 0) {
+            shouldReplace = YES;
+        } else if (self.acceptsNextMediaUrl) {
+            shouldReplace = YES;
+        } else if (incomingScore > self.latestMediaScore) {
+            shouldReplace = YES;
+        } else if (incomingScore == self.latestMediaScore && urlChanged) {
+            shouldReplace = YES;
+        }
+
+        if (!shouldReplace) {
+            return;
+        }
+
         self.latestMediaUrl = urlStr;
+        self.latestMediaScore = incomingScore;
+        self.acceptsNextMediaUrl = NO;
 
         if (!self.overlayWindow) {
             [self setupFloatingUI];
@@ -299,16 +344,16 @@
         if (!self.overlayWindow.isSuspendedHidden) {
             [self showButtonsWithAnimation];
         }
+
+        [self rotateRefreshIcon];
     });
 }
 
 - (void)clearMedia {
     self.latestMediaUrl = nil;
-    if (!self.overlayWindow.isSuspendedHidden) {
-        [self showIdleButtonWithAnimation];
-    } else {
-        [self hideAllFloatingUI];
-    }
+    self.latestMediaScore = NSIntegerMinValue;
+    self.acceptsNextMediaUrl = NO;
+    [self hideButtonsWithAnimation];
 }
 
 - (void)setupFloatingUI {
@@ -354,32 +399,11 @@
             CGFloat ratioY = [self loadPositionRatio];
             CGFloat targetCenterY = ratioY * screenH;
 
-            CGFloat idleD = 38;
-            CGFloat idleSafeY = MIN(MAX(targetCenterY - idleD / 2.0, 40), screenH - idleD - 40);
-            UIButton *idleBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-            idleBtn.frame = CGRectMake(screenW - idleD - 10, idleSafeY, idleD, idleD);
-            idleBtn.backgroundColor = [UIColor colorWithRed:0.96 green:0.96 blue:0.98 alpha:0.94];
-            idleBtn.layer.cornerRadius = idleD / 2.0;
-            idleBtn.layer.masksToBounds = NO;
-            idleBtn.layer.borderWidth = 0.5;
-            idleBtn.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.95].CGColor;
-            idleBtn.layer.shadowColor = [UIColor colorWithWhite:0.0 alpha:0.15].CGColor;
-            idleBtn.layer.shadowOffset = CGSizeMake(0, 2);
-            idleBtn.layer.shadowRadius = 5;
-            idleBtn.layer.shadowOpacity = 1.0;
-            idleBtn.titleLabel.font = [UIFont systemFontOfSize:16];
-            [idleBtn setTitle:@"🎬" forState:UIControlStateNormal];
-
-            UIPanGestureRecognizer *idlePan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onPanIdleButton:)];
-            [idleBtn addGestureRecognizer:idlePan];
-
-            self.overlayWindow.roundIdleButton = idleBtn;
-            [rootVC.view addSubview:idleBtn];
-
             CGFloat btnW = 82;
             CGFloat btnH = 34;
             CGFloat spacing = 8;
-            CGFloat totalH = btnH * 4 + spacing * 3;
+            CGFloat refreshD = 28;
+            CGFloat totalH = btnH * 4 + spacing * 3 + refreshD + 10;
             CGFloat containerSafeY = MIN(MAX(targetCenterY - totalH / 2.0, 40), screenH - totalH - 40);
 
             UIView *container = [[UIView alloc] initWithFrame:CGRectMake(screenW - btnW - 10, containerSafeY, btnW, totalH)];
@@ -427,8 +451,29 @@
                 [btnArray addObject:btn];
             }
 
+            UIButton *refreshButton = [UIButton buttonWithType:UIButtonTypeCustom];
+            refreshButton.frame = CGRectMake((btnW - refreshD) / 2.0, btnH * 4 + spacing * 3 + 8, refreshD, refreshD);
+            refreshButton.backgroundColor = [UIColor colorWithRed:0.96 green:0.96 blue:0.98 alpha:0.94];
+            refreshButton.layer.cornerRadius = refreshD / 2.0;
+            refreshButton.layer.masksToBounds = NO;
+            refreshButton.layer.borderWidth = 0.5;
+            refreshButton.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.85].CGColor;
+            refreshButton.layer.shadowColor = [UIColor colorWithWhite:0.0 alpha:0.12].CGColor;
+            refreshButton.layer.shadowOffset = CGSizeMake(0, 2);
+            refreshButton.layer.shadowRadius = 4;
+            refreshButton.layer.shadowOpacity = 1.0;
+            refreshButton.titleLabel.font = [UIFont systemFontOfSize:17 weight:UIFontWeightMedium];
+            [refreshButton setTitle:@"↻" forState:UIControlStateNormal];
+            [refreshButton setTitleColor:[UIColor colorWithRed:0.3 green:0.35 blue:0.42 alpha:1.0] forState:UIControlStateNormal];
+            [refreshButton addTarget:self action:@selector(btnTouchDown:) forControlEvents:UIControlEventTouchDown];
+            [refreshButton addTarget:self action:@selector(btnTouchUp:) forControlEvents:UIControlEventTouchUpInside | UIControlEventTouchUpOutside | UIControlEventTouchCancel];
+            [refreshButton addTarget:self action:@selector(refreshCandidateTap:) forControlEvents:UIControlEventTouchUpInside];
+
+            [container addSubview:refreshButton];
+
             self.playerButtons = btnArray;
             self.overlayWindow.buttonsContainer = container;
+            self.overlayWindow.refreshButton = refreshButton;
             [rootVC.view addSubview:container];
         } else {
             if (@available(iOS 13.0, *)) {
@@ -454,6 +499,15 @@
     [UIView animateWithDuration:0.1 animations:^{
         btn.transform = CGAffineTransformIdentity;
     }];
+}
+
+- (void)refreshCandidateTap:(UIButton *)sender {
+    UIImpactFeedbackGenerator *feedback = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleMedium];
+    [feedback impactOccurred];
+
+    self.acceptsNextMediaUrl = YES;
+    self.latestMediaScore = NSIntegerMinValue;
+    [self rotateRefreshIcon];
 }
 
 - (void)playerBtnTap:(UIButton *)btn {
@@ -485,22 +539,20 @@
 
 - (void)showButtonsWithAnimation {
     UIView *container = self.overlayWindow.buttonsContainer;
-    UIButton *idleBtn = self.overlayWindow.roundIdleButton;
     if (!container) {
         return;
     }
 
-    [self.overlayWindow syncContainerPositionToIdleButton];
+    CGFloat screenH = self.overlayWindow.bounds.size.height;
+    CGFloat ratio = [self loadPositionRatio];
+    CGFloat targetCenterY = ratio * screenH;
+    CGFloat h = container.frame.size.height;
+    CGFloat safeY = MIN(MAX(targetCenterY - h / 2.0, 40), screenH - h - 40);
 
-    if (idleBtn && !idleBtn.hidden) {
-        [UIView animateWithDuration:0.2 animations:^{
-            idleBtn.alpha = 0.0;
-            idleBtn.transform = CGAffineTransformMakeScale(0.4, 0.4);
-        } completion:^(BOOL finished) {
-            idleBtn.hidden = YES;
-            idleBtn.transform = CGAffineTransformIdentity;
-        }];
-    }
+    CGRect f = container.frame;
+    f.origin.y = safeY;
+    f.origin.x = self.overlayWindow.bounds.size.width - f.size.width - 10;
+    container.frame = f;
 
     if (container.hidden || container.alpha < 0.05) {
         container.hidden = NO;
@@ -512,75 +564,18 @@
     }
 }
 
-- (void)showIdleButtonWithAnimation {
+- (void)hideButtonsWithAnimation {
     UIView *container = self.overlayWindow.buttonsContainer;
-    UIButton *idleBtn = self.overlayWindow.roundIdleButton;
-
-    if (container && !container.hidden) {
-        [UIView animateWithDuration:0.2 animations:^{
-            container.alpha = 0.0;
-            container.transform = CGAffineTransformMakeTranslation(30, 0);
-        } completion:^(BOOL finished) {
-            container.hidden = YES;
-            container.transform = CGAffineTransformIdentity;
-        }];
+    if (!container || container.hidden) {
+        return;
     }
-
-    [self.overlayWindow syncIdleButtonPositionToContainer];
-
-    if (idleBtn) {
-        idleBtn.hidden = NO;
-        idleBtn.transform = CGAffineTransformMakeScale(0.5, 0.5);
-        [UIView animateWithDuration:0.3 delay:0.05 usingSpringWithDamping:0.8 initialSpringVelocity:0.5 options:0 animations:^{
-            idleBtn.alpha = 1.0;
-            idleBtn.transform = CGAffineTransformIdentity;
-        } completion:nil];
-    }
-}
-
-- (void)hideAllFloatingUI {
-    UIView *container = self.overlayWindow.buttonsContainer;
-    UIButton *idleBtn = self.overlayWindow.roundIdleButton;
-
-    [UIView animateWithDuration:0.2 animations:^{
-        if (container) {
-            container.alpha = 0.0;
-        }
-        if (idleBtn) {
-            idleBtn.alpha = 0.0;
-        }
+    [UIView animateWithDuration:0.25 animations:^{
+        container.alpha = 0.0;
+        container.transform = CGAffineTransformMakeTranslation(40, 0);
     } completion:^(BOOL finished) {
-        if (container) {
-            container.hidden = YES;
-        }
-        if (idleBtn) {
-            idleBtn.hidden = YES;
-        }
+        container.hidden = YES;
+        container.transform = CGAffineTransformIdentity;
     }];
-}
-
-- (void)onPanIdleButton:(UIPanGestureRecognizer *)pan {
-    UIView *btn = self.overlayWindow.roundIdleButton;
-    UIView *superView = btn.superview;
-    CGPoint translation = [pan translationInView:superView];
-    CGPoint center = btn.center;
-    center.y += translation.y;
-    btn.center = center;
-    [pan setTranslation:CGPointMake(0.0, 0.0) inView:superView];
-
-    if (pan.state == UIGestureRecognizerStateEnded || pan.state == UIGestureRecognizerStateCancelled) {
-        CGFloat screenH = superView.bounds.size.height;
-        CGFloat h = btn.frame.size.height;
-        CGFloat targetY = MIN(MAX(center.y, 40 + h / 2.0), screenH - 40 - h / 2.0);
-
-        [UIView animateWithDuration:0.25 animations:^{
-            btn.center = CGPointMake(btn.center.x, targetY);
-        } completion:^(BOOL finished) {
-            CGFloat ratio = targetY / screenH;
-            [self savePositionRatio:ratio];
-            [self.overlayWindow syncContainerPositionToIdleButton];
-        }];
-    }
 }
 
 - (void)onPanContainer:(UIPanGestureRecognizer *)pan {
@@ -602,7 +597,6 @@
         } completion:^(BOOL finished) {
             CGFloat ratio = targetY / screenH;
             [self savePositionRatio:ratio];
-            [self.overlayWindow syncIdleButtonPositionToContainer];
         }];
     }
 }
